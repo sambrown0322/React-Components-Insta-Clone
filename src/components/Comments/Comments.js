a165e6e64dc428da4175d0dc2266ea33891eef0b
David Brown <davidbrown@Davids-iMac.local>
2020-07-14 16:52:48 -0600

    mvp

--- src/components/Comments/Comments.js
+++ src/components/Comments/Comments.js
@@ -1,14 +1,17 @@
-import React from 'react';
-import Comment from './Comment';
-import './Comments.css';
+import React from "react";
+import Comment from "./Comment";
+import "./Comments.css";
 
-const Comments = props => {
+const Comments = (props) => {
   // Make sure the parent of Comments is passing the right props!
   const { comments } = props;
 
   return (
     <div>
       {/* map through the comments data array and render a Comment for every comment piece of data */}
+      {comments.map((comment, idx) => {
+        return <Comment comment={comment} key={idx} />;
+      })}
     </div>
   );
 };
--- src/components/Posts/Post.js
+++ src/components/Posts/Post.js
@@ -1,30 +1,23 @@
 // You will add code to this file
-import React from 'react';
-import Comments from '../Comments/Comments';
-import LikeSection from './LikeSection';
-import PostHeader from './PostHeader';
+import React from "react";
+import Comments from "../Comments/Comments";
+import LikeSection from "./LikeSection";
+import PostHeader from "./PostHeader";
 
-const Post = props => {
+const Post = (props) => {
   // Make sure the parent of Post is passing the right props!
-  const { post, likePost } = props
+  const { post, likePost } = props;
 
   return (
-    <div className='post-border'>
-      <PostHeader
-        username={post.username}
-        thumbnailUrl={post.thumbnailUrl}
-      />
-      <div className='post-image-wrapper'>
-        <img
-          alt='post thumbnail'
-          className='post-image'
-          src={post.imageUrl}
-        />
+    <div className="post-border">
+      <PostHeader username={post.username} thumbnailUrl={post.thumbnailUrl} />
+      <div className="post-image-wrapper">
+        <img alt="post thumbnail" className="post-image" src={post.imageUrl} />
       </div>
       {/* LikeSection is missing a prop it needs to function! */}
-      <LikeSection likePost={() => likePost(post.id)} />
+      <LikeSection likePost={() => likePost(post.id)} likeCount={post.likes} />
       {/* Comments also wants its props! */}
-      <Comments />
+      <Comments comments={post.comments} />
     </div>
   );
 };
